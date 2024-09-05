07fd2dc81dda49d6604a8f5609138532ca3d23db
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-09-05 18:13:24 +0530

    Web Analytics Added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Analytics } from "@vercel/analytics/react"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
